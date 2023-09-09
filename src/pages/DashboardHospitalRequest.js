d6b7e169d0f25685a4ea8dff5d63e26ba1ef3b7f
smaasath <99379342+smaasath@users.noreply.github.com>
2023-09-09 22:41:12 +0530

    Blood Bank Req done

--- src/pages/DashboardHospitalRequest.js
+++ src/pages/DashboardHospitalRequest.js
@@ -1,57 +1,152 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import DashboardStructure from '../components/DashboardStructure';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, RefreshControl, View, Text, StyleSheet } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import DashboardStructure from '../components/DashboardStructure';
 import BloodBankRequest from '../components/BloodBankRequest';
+import LoadPage from '../components/LoadPage';
 
-
-
-
-function Children2() {
+function Children2({ Data }) {
   return (
-    <>
-      <View style={{margin:40, marginBottom:5,}}>
-        <Text style={{color:'black'}}>
-          Blood Bank Request
-        </Text>
-      </View>
-      <ScrollView>
+    <View>
+      <Text style={{ margin: 40, marginBottom: 5, color: 'black' }}>
+        Blood Bank Request
+      </Text>
       <View style={styles.childrenconReq}>
-       
-        <BloodBankRequest status={"Normal"} BloodType='B+' BloodQuntity='300ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"Emergency"} BloodType='B+' BloodQuntity='150ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"Urgent"} BloodType='B+' BloodQuntity='200ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"completed"} BloodType='B+' BloodQuntity='600ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"Normal"} BloodType='B+' BloodQuntity='700ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"Emergency"} BloodType='B+' BloodQuntity='500ml' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"Urgent"} BloodType='B+' BloodQuntity='2L' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        <BloodBankRequest status={"completed"} BloodType='B+' BloodQuntity='2L' RequestID='B001' RequestDate='2023-02-02' RequestHospital='Kalmunai Base Hospital'></BloodBankRequest>
-        
+        {Data?.length > 0 ? (
+          Data.map((item, index) => (
+            <BloodBankRequest
+              key={index}
+              status={item.requestStatus}
+              BloodType={item.bloodGroup}
+              BloodQuntity={item.bloodQuantity + 'ml'}
+              RequestID={'B' + item.bloodBankRequestId}
+              RequestDate={item.createdDate}
+              RequestHospital={item.name || 'Blood Bank Request'}
+              location={item.hospitalAddress}
+              bloodBankName={item.bloodBankName}
+              bloodGroup={item.bloodGroup}
+            />
+          ))
+        ) : (
+          <Text style={{color:'red'}}>No requests found</Text>
+        )}
       </View>
-      </ScrollView>
-    </>
-  )
+    </View>
+  );
 }
-function Children1() {
+
+function Children1({ status, setstatus }) {
+  
   return (
-    <View style={{ margin: 30, }}>
+    <View style={{ margin: 30 }}>
       <SelectDropdown
-        data={["All","My", "Normal", "Emergency", "completed", "Urgent"]}
+        data={['All', 'Normal', 'Emergency', 'completed', 'Urgent']}
         buttonStyle={{ borderRadius: 10, width: '40%', height: 40 }}
-        defaultButtonText={["Status"]}
-        buttonTextStyle={{ fontSize: 14, }}
+        defaultButtonText={['Status']}
+        buttonTextStyle={{ fontSize: 14 }}
+        onSelect={(selectedItem) => setstatus(selectedItem)}
       />
     </View>
-  )
+  );
 }
 
-
 export default function DashboardHospitalRequest() {
+  const [bloodBankId, setBloodBankId] = useState('');
+  const [reqArray, setReqArray] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [FilterreqArray, setFilterreqArray] = useState([]);
+  const [status, setstatus] = useState("All");
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchData();
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [bloodBankId]);
+
+
+  useEffect(() => {
+ 
+    if (status === 'All') {
+     
+      setFilterreqArray(reqArray);
+    } else {
+      const filteredData = reqArray.filter((item) => item.requestStatus === status);
+      setFilterreqArray(filteredData);
+    }
+  }, [reqArray, status]);
+
+  async function fetchData() {
+    await retrieveUserSession();
+    await fetchReq();
+  }
+
+  async function retrieveUserSession() {
+    try {
+      const session = await EncryptedStorage.getItem('user_session');
+
+      if (session !== undefined) {
+        const parsedSession = JSON.parse(session);
+        setBloodBankId(parsedSession.bloodBankId);
+      }
+    } catch (error) {
+      console.error('Error retrieving user session:', error);
+    }
+  }
+
+  const fetchReq = async () => {
+    var URL = 'http://localhost:8081/bloodlife/Api/BloodBankRequestApi.php';
+
+    var headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    var Data = {
+      bloodBankId: bloodBankId,
+    };
+
+    fetch(URL, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(Data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setLoader(true);
+        }
+        return response.json();
+      })
+      .then((response) => {
+        if (response.message === 'Request Not Found') {
+          setLoader(true);
+          setReqArray([]);
+        } else {
+          setReqArray(response);
+          setLoader(false);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching requests:', error);
+      });
+  };
+
   return (
-    <>
-      <DashboardStructure children1={<Children1 />} children2={<Children2 />}></DashboardStructure>
-    </>
-  )
+    <ScrollView
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+    >
+      {loader ? (
+        <LoadPage />
+      ) : (
+        <DashboardStructure children1={<Children1 status={status} setstatus={setstatus} />} children2={<Children2 Data={FilterreqArray} />} />
+      )}
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
